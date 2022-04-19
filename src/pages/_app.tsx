29c4531eed74d6ddb85d891851547afe9e17362f
Pedro Soares <pedrosoares@tecgaf.puc-rio.br>
2022-04-19 11:35:48 -0300

    First commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,10 @@
+import { AppProps } from 'next/app'
+import { ReactElement } from 'react'
+
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }: AppProps): ReactElement {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
